e5cf6dc3a747aad12db79ad7de78e6523933f31c
Nguyen Quang Thong <thongnq1@ViettelAD.com>
2018-05-22 15:19:13 +0700

    fist app

---
+++ demo/myapp/db.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://thongnguyenvtel%40live.com:Thongtin6%40@demo-d9xyt.mongodb.net/test?retryWrites=true');
+// var MongoClient = require('mongodb').MongoClient;
+
+// var uri = "mongodb+srv://thongnguyenvtel%40live.com:Thongtin6%40@cluster0.mongodb.net/test";
+// MongoClient.connect(uri, function(err, client) {
+//    const collection = client.db("test").collection("devices");
+//    // perform actions on the collection object
+//    client.close();
+// });
